87bc571c5d51a240c5b69aa35749866fde8cb069
Fatmahzadjali_1@hotmail.com <maathermohammed77@gmail.com>
2023-11-19 11:06:46 +0400

    kkkk

---
+++ Arrays solution/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Arrays
+{
+	 class Program
+	{
+		static void Main()
+		{
+			//string[] names = new string[2];
+			//names[0] = "kk";
+			//names[1] = "aa";
+			//Console.WriteLine(names[0]);
+			//Console.WriteLine(names[1]);
+			
+			//int[][] numbers = new int [4][];
+			//numbers[0] = new int[4] { 1, 2, 8, 2 };
+			//numbers[1] = new int[2] { 2, 3 };
+			//numbers[2] = new int[2] { 9, 2 };
+			//numbers[3] = new int[5] { 2, 4, 6, 7, 4 };
+		
+			int value = Convert.ToInt32(Console.ReadLine());
+			if (value >=75)
+			{
+				Console.WriteLine("Succesful");
+			}
+
+
+        }
+	}
+}
